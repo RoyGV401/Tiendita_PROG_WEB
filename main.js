63a6ac4ced83f49f2a46c9332443699822276877
RoyGV <gomezvalencia.rogelioisaac.proga@gmail.com>
2025-03-13 09:01:50 -0600

    si

--- main.js
+++ main.js
@@ -30,3 +30,55 @@ function doDiv()
     const result = x / y;
     document.getElementById('result_value').innerHTML = result
 }
+
+function enterValue(value)
+{
+    document.getElementById('result_value').innerHTML =  document.getElementById('result_value').innerHTML + value
+
+}
+
+function doEnter() {
+
+    try
+    {
+        const operation = document.getElementById('result_value').innerHTML;
+        const result = eval(operation);
+        if (isNaN(result))
+        {
+            document.getElementById('result_value').innerHTML = "MATH ERROR";
+            setTimeout(
+            function(){
+                document.getElementById('result_value').innerHTML = "";
+
+            },400)
+        }
+        else
+        document.getElementById('result_value').innerHTML = result;
+    }
+    catch
+    {
+        document.getElementById('result_value').innerHTML = "SYNTAX ERROR";
+        setTimeout(
+        function(){
+            document.getElementById('result_value').innerHTML = "";
+
+        },400)
+    }
+   
+    
+}
+
+function doDelete()
+{
+    document.getElementById('result_value').innerHTML = "";
+}
+
+function enterPar1() 
+{    
+    document.getElementById('result_value').innerHTML =  document.getElementById('result_value').innerHTML + "("
+}
+
+function enterPar2() 
+{    
+    document.getElementById('result_value').innerHTML =  document.getElementById('result_value').innerHTML + ")"
+}
