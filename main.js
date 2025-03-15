3af5ea76c40a32688a29ff788a8bb27bf9f337b8
RoyGV <gomezvalencia.rogelioisaac.proga@gmail.com>
2025-03-15 00:05:58 -0600

    Ajustes a los resources + Ajustes en el javascript

--- main.js
+++ main.js
@@ -1,54 +1,91 @@
 const FRUITS = [
     {
-        img:"Cerezas.webp",
-        name:"Cerezas",
+        img:"resources/Cerezas.png",
+        name:"Cereza",
         amount: 80,
         initialStock:80,
         price: 10,
     },
     {
-        img:"platano.webp",
+        img:"resources/platano.png",
         name:"Platano",
         amount: 43,
         initialStock:43,
         price: 15,
     },
     {
-        img:"",
+        img:"resources/manzana.webp",
         name:"Manzana",
         amount: 60,
         initialStock:60,
         price: 12,
     },
     {
-        img:"",
+        img:"resources/guayaba.webp",
         name:"Guayaba",
         amount: 43,
         initialStock:43,
         price: 5,
     },
     {
-        img:"",
+        img:"resources/uvas.png",
         name:"Uvas",
         amount: 200,
         initialStock:200,
         price: 2,
+    },
+    {
+        img:"resources/melon.webp",
+        name:"Melon",
+        amount: 40,
+        initialStock:200,
+        price: 16,
     }
 ]
 
 function reloadAll()
-{
+{    
     for (let index = 0; index < FRUITS.length; index++) {
         const aF = `fruitAmount${index+1}`
         const pF = `fruitPrice${index+1}`
         const iF = `fruitImage${index+1}`
+        const ipF = `input${index+1}`
         const fruitd = document.getElementById(aF);
         fruitd.innerHTML=FRUITS[index].amount
         const fruitdd = document.getElementById(pF);
         fruitdd.innerHTML=FRUITS[index].price
         const fruitddd = document.getElementById(iF);
         fruitddd.src=FRUITS[index].img;
+        const fruitdddd = document.getElementById(ipF);
+        fruitdddd.value="";
     }
+}
+
+function addAll()
+{
+    const table = document.getElementById('calc_table');
+
+    for (let index = 0; index < FRUITS.length; index++) {
+        const fruitRow = document.createElement('tr');
+        fruitRow.innerHTML = `  
+                 
+            <td><img src="${FRUITS[index].img}" alt="${FRUITS[index].name}" class="fruit_image" id="fruitImage${index+1}"></td>
+            <td id="fruitAmount${index+1}"></td>
+            <td id="fruitPrice${index+1}"></td>
+            <td><input type="number" id="input${index+1}" /></td>
+                      
+        `;
+        table.appendChild(fruitRow);
+    }
+    const buttonRow = document.createElement('tr');
+    buttonRow.innerHTML = `
+                    <tr>
+                    <th></th> <th></th> <th></th>
+                    <th><button class="button-5" id="Confirm"  onclick="confirmPurchase()">Confirmar</button></th>
+                  </tr>
+    `;
+    table.appendChild(buttonRow);
+    reloadAll();
 }
 
 function decrementStack(fruit,amountToDecrement)
@@ -78,7 +115,9 @@ function resetStack(fruit)
 function confirmPurchase()
 {
     let total = 0;
-    let amounts = []
+    let amounts = [];
+    let resultString = "";
+
     for (let index = 0; index < FRUITS.length; index++) {
         const aF = `input${index+1}`;
         try{
@@ -91,29 +130,48 @@ function confirmPurchase()
 
         }
     }
+
     let noMistakes = true;
+
+    for (let index = 0; index < FRUITS.length; index++) {
+        try
+        {
+            const amount = amounts[index];
+            if (amount.amount > FRUITS[index].amount)
+            {
+                alert(`La cantidad de ${FRUITS[index].name} excede el inventario`);
+                noMistakes = false;
+                reloadAll();
+                return;
+            }
+        }
+        catch{}
+    }
+
+    if (noMistakes)
     for (let index = 0; index < FRUITS.length; index++) {
         try
         {
             const current = FRUITS[index];
             const amount = amounts[index];
-            if (amount.amount > FRUITS[index].amount)
-            {
-                alert("La cantidad de frutas excede el inventario!");
-                noMistakes = false;
-                reloadAll();
-                return;
-            }
-            else
-            {
-                total += amount.amount * current.price;
-                FRUITS[index].amount += - amount.amount
-                reloadAll();
 
+            
+            total += amount.amount * current.price;
+
+            if (amount.amount > 0)
+            {
+                resultString+=`Producto: ${current.name} || Precio: ${current.price} || Cantidad: ${amount.amount} || Total: ${current.price*amount.amount}\n`
             }
+
+            FRUITS[index].amount += - amount.amount
+            reloadAll();
         }
         catch{}
     }
-    if (noMistakes) alert("Total: "+total)
+
+    resultString += `Total: ${total}`;
+
+    if (noMistakes && total != 0) alert(resultString);
     
 }
+
